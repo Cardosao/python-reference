0c03d20483750e4e34ea0a3ffc67b56a5e9b27e9
Cardosao <fabianocardosao@gmail.com>
2019-09-26 03:02:16 -0300

    add console dumper

--- fundamentos/console_dumper.py
+++ fundamentos/console_dumper.py
@@ -1,11 +1,15 @@
 #!/usr/bin/env python3
 
-class console_dumper:
-    NORMAL = '\033[0m'
-    ERRO = '\033[1;31m'
-    HELP = '\033[1;96m'
+NORMAL = '\033[0m'
+ERRO = '\033[1;31m'
+HELP = '\033[1;96m'
+
+
+class ConsoleDumper:
+
 
     def console_error_dumper(mensagem):
+
         print(ERRO + mensagem + NORMAL)
 
 
